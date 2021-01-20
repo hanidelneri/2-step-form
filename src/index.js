d9f149926d6df4f76031d1bc44e79bd3a5a447e2
Hanie <h7.gharedaghi@gmail.com>
2021-01-20 15:37:47 +0330

    configuring i18n

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import EnglishTranslations from './i18n/en-US.json';
+import GermanTranslations from './i18n/de-DE.json';
+
+i18n.use(initReactI18next).init({
+    resources: {
+        'en-US': {
+            translation: EnglishTranslations,
+        },
+        'de-DE': {
+            translation: GermanTranslations,
+        },
+    },
+    lng: 'en-US',
+    fallbackLng: 'en-US',
+    interpolation: {
+        escapeValue: false,
+    },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
