0aeb1d55f737d4048142a8f8619fe6e7d8836944
Hanie <h7.gharedaghi@gmail.com>
2021-01-20 18:05:03 +0330

    exporting core components

---
+++ src/components/core/index.js
@@ -0,0 +1,5 @@
+export { default as TextInput } from './input';
+export { default as Autocomplete } from './autocomplete';
+export { default as Button } from './button';
+export { useForm } from './form/form.hook';
+export { default as Stepper } from './stepper';
