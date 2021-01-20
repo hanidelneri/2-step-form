89d5c8792c16e3250023581597b4121372f25fed
Hanie <h7.gharedaghi@gmail.com>
2021-01-20 19:15:33 +0330

    removing unused files

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import EnglishTranslations from './i18n/en-US.json';
 import GermanTranslations from './i18n/de-DE.json';
 
@@ -30,8 +28,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
